7298c7857d267a0aa459ce573e6152e53bd59f23
Skylar <skylar.som@gmail.com>
2019-06-25 01:12:38 -0400

    Change load order

--- src/main/java/net/allthemods/modularmachineryadapters/ModularMachineryAdapters.java
+++ src/main/java/net/allthemods/modularmachineryadapters/ModularMachineryAdapters.java
@@ -17,7 +17,7 @@ public class ModularMachineryAdapters {
     public static final String MOD_ID = "modularmachineryadapters";
     public static final String MOD_NAME = "ModularMachineryAdapters";
     public static final String VERSION = "1.0.0";
-    public static final String DEPEND = "required-before:modularmachinery;after:forestry";
+    public static final String DEPEND = "required-after:modularmachinery;after:forestry";
 
     @SubscribeEvent
     public static void registerAdapters(RegistryEvent.Register<RecipeAdapter> event) {
